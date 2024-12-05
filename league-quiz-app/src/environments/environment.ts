46e542eb4dbd229373ac280abd00ac1057d81bca
Vidya Gangaswamy <vgangaswamy@hawk.iit.edu>
2024-12-04 22:34:00 -0600

    API Keys Updation

--- league-quiz-app/src/environments/environment.ts
+++ league-quiz-app/src/environments/environment.ts
@@ -1,10 +1,4 @@
 export const environment = {
-<<<<<<< HEAD
     production: false,
-    openaiApiKey: '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',
+    openaiApiKey: process.env['OPENAI_API_KEY'] || '',
   };
-=======
-  production: false,
-  openaiApiKey: 'OPENAI_API_KEY'
-};
->>>>>>> fe442f6fa184a271feb3e83196f0390a46bd57d9
